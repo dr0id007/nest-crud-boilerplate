923681bd312636cafb742a719cd4b0e209818c7f
dr0id007 <devarora745@gmail.com>
2021-08-07 03:11:58 +0530

    modules updated and swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Api Boilerplate')
+    .setDescription('api description')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
